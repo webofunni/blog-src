2124acbbdfac6b466f98dad0d6d7e45d7c59851b
Unni <unnikrishnan.a@gmail.com>
2014-09-03 05:44:58 +0530

    adding disqus

--- publishconf.py
+++ publishconf.py
@@ -16,7 +16,7 @@ DELETE_OUTPUT_DIRECTORY = True
 # Uncomment following line for absolute URLs in production:
 #RELATIVE_URLS = False
 
-DISQUS_SITENAME = ""
+DISQUS_SITENAME = "mutexespellican"
 GOOGLE_ANALYTICS = ""
 
 DEBUG = False
